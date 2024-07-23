fcca7c6e2bb87ee19561405ce8f64669b619caeb
dacer521 <140218311+dacer521@users.noreply.github.com>
2024-07-23 15:33:03 -0400

    Add files via upload

--- homePage/script.js
+++ homePage/script.js
@@ -1,15 +1,44 @@
-{
-    const nav = document.querySelector(".nav");
-    let lastScrollY = window.scrollY;
-  
-    window.addEventListener("scroll", () => {
-      if (lastScrollY < window.scrollY) {
-        nav.classList.add("nav--hidden");
-      } else {
-        nav.classList.remove("nav--hidden");
+window.onload = function() {
+  var cello = document.getElementById('cello');
+  var mtg = document.getElementById('mtg');
+  var book = document.getElementById('book');
+
+  cello.style.display = "none";
+  mtg.style.display = "none";
+  book.style.display = "none";
+};
+  function showHide(elementId) {
+    var element = document.getElementById(elementId);
+    if (element.style.display === "none") {
+
+      element.style.display = "inline";
+
+      if (elementId === 'cello') {
+        element.style.width = "30%";
+        element.style.height = "40%";
+
+
+        mtg.style.display = "none";
+        book.style.display = "none";
+
+
+      } else if (elementId === 'mtg') {
+        element.style.width = "33.3%";
+        element.style.height = "80%";
+
+        cello.style.display = "none";
+        book.style.display = "none";
+
+
+      } else if (elementId === 'book') {
+        element.style.width = "30%";
+        element.style.height = "70%";
+
+        cello.style.display = "none";
+        mtg.style.display = "none";
+
       }
-  
-      lastScrollY = window.scrollY;
-    });
+    } else {
+      element.style.display = "none";
+    }
   }
-  
