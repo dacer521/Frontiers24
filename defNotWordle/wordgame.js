53365b03610662d9ca0834cdb8141292fa336cec
Manuel <dmanuel@wpi.edu>
2024-07-29 09:37:49 -0400

    hi

---
+++ defNotWordle/wordgame.js
@@ -0,0 +1,35 @@
+let popularWords;
+let allWords;
+
+function loadGame() {
+    Promise.all([
+        fetch('./popular.txt')
+            .then(response => response.text())
+            .then(text => {
+                popularWords = text.split("\n");
+                popularWords.pop(); // Remove the '' at the end
+            })
+            .catch(error => {
+                console.error('Error fetching words: ', error);
+            }),
+        fetch("./enable1.txt")
+            .then(response => response.text())
+            .then(text => {
+                allWords = text.split("\n");
+                allWords.pop(); // Remove the '' at the end
+            })
+            .catch(error => {
+                console.error('Error fetching words: ', error);
+            })]).finally(wordsLoaded);
+}
+
+function wordsLoaded() {
+    console.log(`Loaded ${popularWords.length} popular words and ${allWords.length} dictionary words!`)
+}
+
+// TODO: copy randInt
+// TODO: write function isWord(word)
+
+// For checking word:  json.hasOwnProperty("programming")
+// For array of words: let arr = Object.keys(json)
+// For a random word:  let word = arr[randInt(0, arr.length - 1)];
